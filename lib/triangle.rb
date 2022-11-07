3a41879844643c01ce17b879dd06390d0f7b1d01
nxoo <nxoo.dev@gmail.com>
2022-11-07 05:45:59 +0300

    test passed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor = :a, :b,:c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid =(@a && @b && @c) > 0 &&
+      @b + @c > @a &&
+      @a + @b > @c &&
+      @a + @c > @b &&
+      @b + @c > @a
+    raise TriangleError unless valid
+    if @a.eql?(@b) && @b.eql?(@c)
+      :equilateral
+    elsif @a.eql?(@b) || @b.eql?(@c) || @a.eql?(@c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "Error"
+  end
+
 end
